d36cd5209f266ba1c8d9e8cd9ea4d2b6d84f2786
Yajahira Velazquez <yari@yvelazquez.com>
2022-05-24 17:10:07 -0400

    refactored renderIngs fx and added chart fx

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 //--------------------GLOBAL VARIABLES/IMPORTS
-let voteCount = 25;
+let voteCount = 5;
 let allProducts = [];
 
 //--------------------DOM REF
@@ -9,6 +9,8 @@ let imgContainer = document.getElementById('img-container');
 let img1 = document.getElementById('img1');
 let img2 = document.getElementById('img2');
 let img3 = document.getElementById('img3');
+
+let ctx = document.getElementById('myChart').getContext('2d');
 
 let showResultsBtn = document.getElementById('resultsButton');
 let resultsList = document.getElementById ('resultsList');
@@ -48,23 +50,30 @@ function getRandomIndex(){
   return Math.floor(Math.random()*allProducts.length);
 }
 
+let productIndex = [];
+
 function renderImgs(){
 
-  let productOneIndex = getRandomIndex();
-  let productTwoIndex= getRandomIndex();
-  let productThreeIndex= getRandomIndex();
-
-  //validation to make sure nums are unique; try splice
-
-  while(productOneIndex === productTwoIndex){
-    productTwoIndex = getRandomIndex();
-
-
+  while(productIndex.length < 6){
+    let randomIndex = getRandomIndex();
+    if(!productIndex.includes(randomIndex)){
+      productIndex.push(randomIndex);
+    }
   }
 
-  while(productTwoIndex === productThreeIndex || productOneIndex === productThreeIndex){
-    productThreeIndex = getRandomIndex();
-  }
+  let productOneIndex = productIndex.shift();
+  let productTwoIndex= productIndex.shift();
+  let productThreeIndex= productIndex.shift();
+
+  // //validation to make sure nums are unique; try splice
+
+  // while(productOneIndex === productTwoIndex){
+  //   productTwoIndex = getRandomIndex();
+  // }
+
+  // while(productTwoIndex === productThreeIndex || productOneIndex === productThreeIndex){
+  //   productThreeIndex = getRandomIndex();
+  // }
 
   img1.src = allProducts[productOneIndex].photo;
   img1.alt = allProducts[productOneIndex].name;
@@ -86,12 +95,82 @@ renderImgs();
 //--------------------EVENT LISTENERS
 imgContainer.addEventListener('click', handleClick);
 showResultsBtn.addEventListener('click', handleShowResults);
+
+function renderChart(){
+
+  let productsType = [];
+  let productVotes = [];
+  let productViews = [];
+
+  for (let i=0; i < allProducts.length; i++){
+    productsType.push(allProducts[i].name);
+    productVotes.push(allProducts[i].votes);
+    productViews.push(allProducts[i].views);
+  }
+  let myProductChart = {
+    type: 'bar',
+    data: {
+      labels: productsType,
+      datasets: [{
+        label: '# of Votes',
+        data: productVotes,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      },
+      {
+        label: '# of Views',
+        data: productViews,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 4
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  };
+
+  const myChart = new Chart(ctx, myProductChart);
+}
 //--------------------EVENT HANDLERS
 function handleClick(event){
   voteCount--;
   let imgClicked = event.target.alt;
 
-  for(let i = 0; i<allProducts.length; i++){
+  for(let i = 0; i < allProducts.length; i++){
     if(imgClicked === allProducts[i].name){
       allProducts[i].votes++;
     }
@@ -110,11 +189,13 @@ function handleClick(event){
 
 function handleShowResults(){
 
-  for(let i =0; i< allProducts.length; i++){
-    let liElem = document.createElement('li');
-    liElem.textContent = `${allProducts[i].name} was seen ${allProducts[i].views} times and chosen ${allProducts[i].votes} times.`;
-    resultsList.appendChild(liElem);
-  }
+  renderChart();
+
+  // for(let i =0; i< allProducts.length; i++){
+  //   let liElem = document.createElement('li');
+  //   liElem.textContent = `${allProducts[i].name} was seen ${allProducts[i].views} times and chosen ${allProducts[i].votes} times.`;
+  //   resultsList.appendChild(liElem);
+  // }
 
   showResultsBtn.removeEventListener('click', handleShowResults);
   showResultsBtn.hidden=true;
